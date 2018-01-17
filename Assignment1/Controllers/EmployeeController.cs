45de3d90263ac16e8e68ab7386958a3ef8e135b0
Joel Gregory <joelgregory7@gmail.com>
2018-01-17 16:45:17 -0500

    Assignment 1 complete

---
+++ Assignment1/Controllers/EmployeeController.cs
@@ -0,0 +1,14 @@
+namespace Assignment1.Controllers {
+    public class EmployeeController {
+        public string Index() {
+           return "Hello from Employee"; 
+        }
+        public string Name() {
+            return "Joel";
+        }
+
+        public string Country() {
+            return "USA";
+        }
+    }
+}
---
+++ Assignment1/Controllers/HomeController.cs
@@ -0,0 +1,7 @@
+namespace Assignment1.Controllers {
+    public class HomeController {
+        public string Index() {
+            return "Hello from the controller";
+        }
+    }
+}
